79023c24f04cadf18a184d518d72131db686e165
demandosigno <34424426+demandosigno@users.noreply.github.com>
2024-01-12 10:17:38 +0900

    ビット演算子、演算子の優先順位と結合測

---
+++ chap03/scripts/bit.js
@@ -0,0 +1,4 @@
+x = ~10;
+console.log(x);
+y = 10 >> 1;
+console.log(y);
---
+++ chap03/scripts/convert_bool.js
@@ -0,0 +1,5 @@
+let result = '';
+console.log(typeof (!!result));
+console.log(!!result);
+let result2 = 1;
+console.log(!!result2);
