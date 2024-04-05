6a5bf9f0f299150b71761aac4b865e9344e68bfb
demandosigno <34424426+demandosigno@users.noreply.github.com>
2024-04-05 21:21:43 +0900

    関数

---
+++ chap06/scripts/func_arrow.js
@@ -0,0 +1,11 @@
+// let getTriangleArea = (base, height) => {
+//   return base * height / 2;
+// };
+
+let getTriangleArea = (base, height) => base * height / 2;
+
+console.log(getTriangleArea(10, 4));
+
+let getCircleArea = radius => (radius ** 2) * Math.PI;
+
+console.log(getCircleArea(10));
